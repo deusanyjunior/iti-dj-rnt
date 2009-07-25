d527c0ac3e35e7f6c91cd172fb19d8e9cc4dc6ab
rennandf <rennandf@ff4f06be-720d-11de-a9df-91707ec8e704>
2009-07-25 20:29:03 +0000

    Refactor de codigo

--- trunk/src/ppmc/api/Main.java
+++ trunk/src/ppmc/api/Main.java
@@ -13,10 +13,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        Contexto.debug = true;
         long time = System.currentTimeMillis();
-//        Encoder encoder = new Encoder(8, 1, "scriptTabelasReduzidoPostgres.sql", "comp.ppm");
-//        Decoder decoder = new Decoder(8, 1, "comp.ppm","strp.sql");
-        Encoder encoder = new Encoder(8, 6, "Selecao_-_Jailton_Maciel.rar", "comp.ppm");
-        Decoder decoder = new Decoder(8, 6, "comp.ppm","sjm.rar");
+        Encoder encoder = new Encoder(8, 1, "scriptTabelasReduzidoPostgres.sql", "comp.ppm");
+        Decoder decoder = new Decoder(8, 1, "comp.ppm","strp.sql");
+//        Encoder encoder = new Encoder(8, 1, "Selecao_-_Jailton_Maciel.rar", "comp.ppm");
+//        Decoder decoder = new Decoder(8, 1, "comp.ppm","sjm.rar");
         System.out.println(System.currentTimeMillis() - time);
         
     }
