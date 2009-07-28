d8ae404e5541aef13564a94cb993160a8cfa446a
rennandf <rennandf@ff4f06be-720d-11de-a9df-91707ec8e704>
2009-07-28 00:51:59 +0000

    PPMC com ED = Map<String, Map<Integer, Integer>> ao invés de Map<String, int[]>; Muito menos memória ocupada e muito mais lento

--- src/ppmc/api/Main.java
+++ src/ppmc/api/Main.java
@@ -13,10 +13,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        Contexto.debug = true;
         long time = System.currentTimeMillis();
-        Encoder encoder = new Encoder(8, 1, "scriptTabelasReduzidoPostgres.sql", "comp.ppm");
-        Decoder decoder = new Decoder(8, 1, "comp.ppm","strp.sql");
-//        Encoder encoder = new Encoder(8, 1, "Selecao_-_Jailton_Maciel.rar", "comp.ppm");
-//        Decoder decoder = new Decoder(8, 1, "comp.ppm","sjm.rar");
+//        Encoder encoder = new Encoder(8, 1, "scriptTabelasReduzidoPostgres.sql", "comp.ppm");
+//        Decoder decoder = new Decoder(8, 1, "comp.ppm","strp.sql");
+        Encoder encoder = new Encoder(8, 1, "100-1.dat", "comp.ppm");
+        Decoder decoder = new Decoder(8, 1, "comp.ppm","100-1(2).dat");
+//        Encoder encoder = new Encoder(8, 4, "Selecao_-_Jailton_Maciel.rar", "comp.ppm");
+//        Decoder decoder = new Decoder(8, 4, "comp.ppm","sjm.rar");
         System.out.println(System.currentTimeMillis() - time);
         
     }
